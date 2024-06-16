35da412ad3c93c35a78d7a72be3579ca17b61cac
Collins Swai <swailerc23@gmail.com>
2024-06-16 17:09:27 +0300

    Fixed firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:mzizi_school_interview/presentation/splash_screen.dart';
 import 'package:mzizi_school_interview/presentation/home_screen.dart';
 import 'package:mzizi_school_interview/presentation/login_screen.dart';
@@ -8,40 +8,42 @@ import 'package:mzizi_school_interview/presentation/login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      setState(() {
+        _user = user;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthCheck(),
-    );
+    if (_user == null) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      );
+    } else {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: _user == null ? LoginScreen() : HomeScreen(),
+      );
+    }
   }
 }
 
-class AuthCheck extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return SplashScreen(); // Show splash screen while waiting
-        } else if (snapshot.hasData) {
-          return HomeScreen(); // User is logged in
-        } else {
-          return LoginScreen(); // User is not logged in
-        }
-      },
-    );
-  }
-}
